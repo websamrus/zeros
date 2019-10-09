ff185ede0dc7d82739a290069f7a8d6d8acb52d7
websamrus <websamrus@gmail.com>
2019-10-09 13:12:37 +0300

    feat: add my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function zeros(expression) {
-  // your solution
-}
+  let arr = expression.split('*');
+  let zero=0;
+  let odd=false;
+  
+  for(let i=0; i<arr.length; i++){
+      let count = arr[i].match(/!/g).length;
+      let numb = arr[i].match(/[0-9]/g).join('');
+      if(numb % 2 == 0 && count == 2 || count == 1) odd=true;
+        for(let j=numb; j>=count; j-=count){
+          if (j % 5 == 0 ) zero++;
+          if (j % 25 == 0) zero++;
+          }
+    }
+      if (!odd) zero=0;
+      return zero;
+  }
+
+  
+
